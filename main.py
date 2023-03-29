ed0b6fc797df8c540ccaf9556a20dcdd16680b39
Pedro <ramses2h@gmail.com>
2023-03-29 18:18:42 -0400

    Ver2 Mod widget position

--- main.py
+++ main.py
@@ -2,25 +2,29 @@
 # I decided to use in this version Tkinter as a UI and Pillow as the Image Manipulator
 # Remember Pillow supports PNG, JPEG, PPM, GIF, TIFF, and BMP
 
-
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog as fd
 from PIL import Image, ImageTk, ImageDraw, ImageFont, ImageGrab
-
+import os
 
 # ----- Variables  Area ----- #
 
+
+# Define Constants
+
+DARKGREY = '#2F4F4F'
+GREY = '#696969'
 BACK_COLOR = '#ffffff'
-GREY = '#EAEAEA'
+L_GREY = '#EAEAEA'
 MIDGREY = '#A6A6A6'
-DARKGREY = '#333333'
+D_GREY = '#333333'
 LIGHTBLUE = '#0078D7'
 SKY_BLUE = '#00B7C3'
-FONT_1 = ('times', 24, 'bold')
-FONT_2 = ('courier', 14, 'bold')
-program_is_on = False
+FONT_1 = ('times', 30, 'bold')
+FONT_2 = ('courier', 14)
+FONT_3 = ('times', 14, 'bold')
 
 # ----- Function definition  Area ----- #
 
@@ -125,45 +129,45 @@ def restart():
 # Todo 1 configure the starting presentation window.
 window = tk.Tk()
 window.title('App for Adding a Watermark to a Photo by Pedro Hernandez')
-window.geometry('600x400')
+window.geometry('400x400')
 window.config(background=BACK_COLOR, highlightbackground=DARKGREY, highlightthickness=7)
 
 
 # Todo 2 Add Initial Label.
 # add a Title Label
-title_label = Label(window, text='Please, Upload Your Image', width=30, font=FONT_1, bg=BACK_COLOR)
+title_label = Label(window, text='Please, Choose Your Image', width=30, font=FONT_1, bg=BACK_COLOR)
 title_label.pack(pady=20)
 
 # Todo 3 add the image browsing/loading button.
 # Create a button to display the text of entry widget
-button = ttk.Button(window, text="Upload Image", command=select_files, padding=10)
-button.pack(pady=20)
+button = ttk.Button(window, text="Choose Image", command=select_files, padding=4)
+button.pack(pady=5)
 
 # Todo 4 Add a input box for watermark text.
 # add a box for input the watermark text
 water_label = Label(window, text="Add the Watermark text", font=FONT_2, bg=BACK_COLOR)
-water_label.pack()
-texto = Entry(window, font=FONT_2)
-texto.pack()
+water_label.pack(pady=10)
+texto = Entry(window, font=FONT_2, width=30)
+texto.pack(pady=3)
 
 # Todo 5  Add a button for download the final product and a box foe adding the new location for the file.
-water_button = ttk.Button(window, text="Apply the Watermark", command=create_watermark, padding=10)
-water_button.pack(pady=10)
+water_button = ttk.Button(window, text="Apply the Watermark", command=create_watermark, padding=5)
+water_button.pack(pady=5)
 
-want_to_save = ttk.Button(window, text="Save Your Photo", command=photo_saver, padding=10)
-want_to_save.pack(side=LEFT, anchor=N, expand=True, pady=10, padx=10)
+want_to_save = ttk.Button(window, text="Save Your Photo", command=photo_saver, padding=3)
+want_to_save.pack(side=TOP, anchor=N, expand=True, pady=10)
 
 # todo 6 add a button for leaving the app or to watermark another photo.
-leave_fmr = tk.Frame(window, relief=tk.RAISED, bd=4)
+leave_fmr = tk.Frame(window, relief=tk.RAISED, bd=3)
 leave = ttk.Button(leave_fmr, text="QUIT", command=quit)
-leave.pack(side=LEFT, anchor=N, expand=True, padx=0, pady=0)
-leave_fmr.pack(side=LEFT, anchor=N, expand=True, padx=10, pady=10)
+leave.pack(side=BOTTOM, anchor=N, expand=True)
+leave_fmr.pack(side=BOTTOM, anchor=N, expand=True)
 
 # todo 7 add a button for restarting the program
-one_fmr = tk.Frame(window, relief=tk.RAISED, bd=4)
+one_fmr = tk.Frame(window, relief=tk.RAISED, bd=3)
 one_more = ttk.Button(one_fmr, text="Restart", command=restart)
-one_more.pack(side=LEFT, anchor=N, expand=True)
-one_fmr.pack(side=LEFT, anchor=N, expand=True)
+one_more.pack(side=RIGHT, anchor=N, expand=True)
+one_fmr.pack(side=RIGHT, anchor=N, expand=True)
 
 
 window.pack_propagate(False)  # disable window size propagation
